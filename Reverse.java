ebe11644dd8982165914b5aa5c2b5af31e6817f5
AnjaliKachhi <anjalikachhi69972@gmail.com>
2025-05-11 21:59:30 +0530

    .

--- Reverse.java
+++ Reverse.java
@@ -5,7 +5,7 @@ public class Reverse {
     System.out.println("enter the size of array: ");
 
 int n=sc.nextInt();
-System.out.println("Enter the elements of array");
+System.out.println("Enter the elements : ");
 int[] arr=new int[n];
 for(int i=0;i<n;i++){
 arr[i]=sc.nextInt();
