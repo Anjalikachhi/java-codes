03e9e40d8a7e142c3ca34bc4f9a026b381cdfad0
AnjaliKachhi <anjalikachhi69972@gmail.com>
2025-05-12 13:24:16 +0530

    dutch national flag problem

--- Dutch.java
+++ Dutch.java
@@ -1,5 +1,34 @@
 public class Dutch {
     public static void main(String[] args) {
-        
+        int[] arr ={2,0,1,0,1,2,1};
+        int low=0,mid=0,high=arr.length-1;
+        while (mid<=high) {
+            switch(arr[mid]){
+                case 0:
+                int  temp=arr[low];
+                arr[low]=arr[mid];
+                arr[mid]=temp;
+                mid++;
+                low++;
+break;
+                case 1:
+                mid++;
+                break;
+case 2:
+int tempo=arr[mid];
+arr[mid]=arr[high];
+arr[high]=tempo;
+high--;
+break;
+default: 
+System.out.println("input array wrong");
+
+            
+            }
+            
+        }
+        for(int i:arr){
+            System.out.println(i);
+        }
     }
 }
